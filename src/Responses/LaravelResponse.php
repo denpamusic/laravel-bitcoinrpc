e9a5a32e64c28eb6542e34f0e5f40c4fe9a079c9
Denis Paavilainen <denpa@denpa.pro>
2018-10-04 19:37:43 +0300

    Apply fixes from StyleCI (#10)

--- src/Responses/LaravelResponse.php
+++ src/Responses/LaravelResponse.php
@@ -4,7 +4,8 @@ namespace Denpa\Bitcoin\Responses;
 
 use Illuminate\Support\Collection;
 
-class LaravelResponse extends BitcoindResponse {
+class LaravelResponse extends BitcoindResponse
+{
     /**
      * Get result as Laravel Collection.
      *
