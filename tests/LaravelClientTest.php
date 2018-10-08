d27e0ad3efc566a5d23a1ec8ba41aa0b8db17932
denpamusic <denpa@netfleet.space>
2018-10-09 01:06:48 +0300

    Added tests.

---
+++ tests/LaravelClientTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use Denpa\Bitcoin\LaravelClient;
+use Denpa\Bitcoin\Responses\LaravelResponse;
+
+class LaravelClientTest extends TestCase
+{
+    /**
+     * Test that handler getter returns correct response class.
+     *
+     * @return void
+     */
+    public function testResponse()
+    {
+        $fake = new FakeClient([]);
+
+        $this->assertEquals(
+            'Denpa\\Bitcoin\\Responses\\LaravelResponse',
+            $fake->getResponseHandler()
+        );
+    }
+
+    /**
+     * Test on() method call without laravel-zeromq package installed.
+     *
+     * @return void
+     */
+    public function testSubsribeWithoutZMQ()
+    {
+        $this->expectException(BadMethodCallException::class);
+        $this->expectExceptionMessage(
+            'ZeroMQ support is not available, because '.
+            '"denpa/laravel-zeromq" package is not installed. '.
+            'Please install it.'
+        );
+
+        $this->bitcoind()->on('hashblock', function () {});
+    }
+}
+
+class FakeClient extends LaravelClient
+{
+    public function getResponseHandler()
+    {
+        return parent::getResponseHandler();
+    }
+}
---
+++ tests/LaravelResponseTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Collection;
+use Denpa\Bitcoin\Responses\LaravelResponse;
+
+class LaravelResponseTest extends TestCase
+{
+    /**
+     * Set up environment.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $data = [
+            'result' => [
+                'foo' => [
+                    'bar' => 'baz',
+                ],
+            ],
+        ];
+
+        $this->response = $this
+            ->getMockBuilder('Psr\Http\Message\ResponseInterface')
+            ->getMock();
+
+        $this->response->expects($this->once())
+            ->method('getBody')
+            ->willReturn(json_encode($data));
+    }
+
+    /**
+     * Test getting laravel collection from response data.
+     *
+     * @return void
+     */
+    public function testCollection()
+    {
+        $response = new LaravelResponse($this->response);
+
+        $foo = $response->collect('foo');
+        $this->assertInstanceOf(Collection::class, $foo);
+        $this->assertEquals('baz', $foo->get('bar'));
+    }
+}
